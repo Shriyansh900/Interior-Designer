2c1550bf3241783f873b0fefac663c83eba0272a
Shriyansh Ozarkar <sozarkar7@gmail.com>
2025-06-06 02:41:34 +0530

    backend minor fixes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://elegance-interiors.vercel.app' 
+    ? 'https://interior-designer-gold.vercel.app/' 
     : 'http://localhost:5173',
   credentials: true
 }));
